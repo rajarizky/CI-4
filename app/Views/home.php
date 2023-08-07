81c60f8fdbc1aca82a599434f2ebc3122067980e
raja_rizky <rajarizky08@gmail.com>
2023-08-07 11:53:32 +0700

    upload CI-4

---
+++ app/Views/home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=echo, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum id asperiores impedit repudiandae consectetur! Exercitationem omnis consequuntur dicta ducimus consequatur laborum animi labore pariatur placeat eius, veritatis itaque iure aperiam?</h1>
+</body>
+
+</html>
